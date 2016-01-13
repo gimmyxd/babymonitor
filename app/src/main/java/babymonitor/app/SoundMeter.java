3fd3c32f3e0befd63f8cb7f04f1820ac57210254
PopescuGheorghe <gimmy.xdd@gmail.com>
2016-01-13 14:18:05 +0200

    small updates

--- app/src/main/java/babymonitor/app/SoundMeter.java
+++ app/src/main/java/babymonitor/app/SoundMeter.java
@@ -6,10 +6,8 @@ import java.io.IOException;
 
 
 public class SoundMeter {
-    static final private double EMA_FILTER = 0.6;
 
     private MediaRecorder mRecorder = null;
-    private double mEMA = 0.0;
 
     public void start()  {
         if (mRecorder == null) {
@@ -29,7 +27,6 @@ public class SoundMeter {
                 e.printStackTrace();
             }
             mRecorder.start();
-            mEMA = 0.0;
         }
     }
 
@@ -47,17 +44,5 @@ public class SoundMeter {
         else
             return 1;
     }
-    public double getAmplitude() {
-        if (mRecorder != null)
-            return  (mRecorder.getMaxAmplitude()/2700.0);
-        else
-            return 0;
 
-    }
-
-    public double getAmplitudeEMA() {
-        double amp = getAmplitude();
-        mEMA = EMA_FILTER * amp + (1.0 - EMA_FILTER) * mEMA;
-        return mEMA;
-    }
 }
